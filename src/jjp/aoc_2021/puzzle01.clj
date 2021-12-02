a1a4b21f55da7bb8bcff785824333581fe181ac3
Jonathan Prettyman <jonathanprettyman@Jons-Computer.local>
2021-12-01 23:54:15 -0800

    Puzzle 1 done!

--- src/jjp/aoc_2021/puzzle01.clj
+++ src/jjp/aoc_2021/puzzle01.clj
@@ -28,4 +28,22 @@
   (map #(Long/parseLong %) (line-seq (io/reader (io/resource "puzzle_input_01.txt")))))
 
 (count (filter #(> (second %) (first %)) (map vector input (rest input))))
+;; solution to part 1 - just a sliding window of two
 ;; => 1184
+
+;; part 2 is sliding window of 3
+
+(let [s1 demo-input
+      s2 (rest s1)
+      s3 (rest s2)
+      sums (map + s1 s2 s3)]
+  (count (filter #(> (second %) (first %)) (map vector sums (rest sums)))))
+;; => 5
+
+(let [s1 input
+      s2 (rest s1)
+      s3 (rest s2)
+      sums (map + s1 s2 s3)]
+  (count (filter #(> (second %) (first %)) (map vector sums (rest sums)))))
+;; a solution to part 2 slide over input to build 3-tuples, sum them, then slide over that to map
+;; => 1158
