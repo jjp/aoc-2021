f1fa55a7bfb53219bb3dc941c988acd731a96b3d
Jonathan Prettyman <jonathanprettyman@Jons-Computer.local>
2021-12-01 23:37:59 -0800

    Solved part 1 of puzzle 1

--- src/jjp/aoc_2021/puzzle01.clj
+++ src/jjp/aoc_2021/puzzle01.clj
@@ -1,1 +1,31 @@
-(ns jjp.aoc-2021.puzzle01)
+(ns jjp.aoc-2021.puzzle01
+  (:require [clojure.java.io :as io]))
+
+
+(def demo-input [199
+                 200
+                 208
+                 210
+                 200
+                 207
+                 240
+                 269
+                 260
+                 263])
+
+(count (filter #(> (second %) (first %)) (map vector demo-input (rest demo-input))))
+;; => ([199 200]
+;;     [200 208]
+;;     [208 210]
+;;     [210 200]
+;;     [200 207]
+;;     [207 240]
+;;     [240 269]
+;;     [269 260]
+;;     [260 263])
+
+(def input
+  (map #(Long/parseLong %) (line-seq (io/reader (io/resource "puzzle_input_01.txt")))))
+
+(count (filter #(> (second %) (first %)) (map vector input (rest input))))
+;; => 1184
