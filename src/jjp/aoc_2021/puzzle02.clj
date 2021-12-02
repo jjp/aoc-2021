0036fa0ea2cc49b6875056dede85f0f8d22bde06
Jonathan Prettyman <jonathanprettyman@Jons-Computer.local>
2021-12-02 14:09:12 -0800

    Puzzle 2 part 2 done!

--- src/jjp/aoc_2021/puzzle02.clj
+++ src/jjp/aoc_2021/puzzle02.clj
@@ -62,3 +62,36 @@ forward 2"
       position   (* horizontal depth)]
   [horizontal up down depth position])
 ;; => [2033 -1125 1875 750 1524750]
+
+;; need to go to bed now
+;; so, with part 2 instructions, I need to "slide" over the input I think instead of just filtering and
+;; summing
+
+
+(loop [[action & remaining] demo-input
+       horizontal 0
+       aim 0
+       depth 0]
+  (let [[command value] (str/split (or action "break 0") #" ")
+        value (Long/parseLong value)
+        ]
+;;     (println {:command command :value value :state {:horizontal horizontal :aim aim :depth depth}})
+    (cond (= command "forward") (recur remaining (+ horizontal value) aim (+ depth (* aim value)))
+        (= command "down") (recur remaining horizontal (+ aim value) depth)
+        (= command "up") (recur remaining horizontal (- aim value) depth)
+        :else [horizontal aim depth (* horizontal depth)]
+        )))
+;; => [15 10 60 900]
+
+(loop [[action & remaining] input
+       horizontal 0
+       aim 0
+       depth 0]
+  (let [[command value] (str/split (or action "break 0") #" ")
+        value (Long/parseLong value)]
+;;     (println {:command command :value value :state {:horizontal horizontal :aim aim :depth depth}})
+    (cond (= command "forward") (recur remaining (+ horizontal value) aim (+ depth (* aim value)))
+          (= command "down") (recur remaining horizontal (+ aim value) depth)
+          (= command "up") (recur remaining horizontal (- aim value) depth)
+          :else [horizontal aim depth (* horizontal depth)])))
+;; => [2033 750 783289 1592426537]
