4c251f7097ed524992ee5dbb3e305babd6d942c3
Jonathan Prettyman <jonathanprettyman@Jons-Computer.local>
2021-12-04 20:46:43 -0800

    Starting puzzle04 game building

---
+++ src/jjp/aoc_2021/puzzle04.clj
@@ -0,0 +1,49 @@
+(ns jjp.aoc-2021.puzzle04
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(def demo-input
+  "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"
+  )
+
+(def rolls )
+
+(defn rolls [input]
+  (as-> input s
+    (str/split s #"\n")
+    (first s)
+    (str/split s #",")
+    (map #(Long/parseLong %) s)))
+
+(defn boards [input]
+  (as-> input s
+    (str/split s #"\n\n")
+    (rest s)
+    )
+  )
+
+(defn build-game [input]
+  (let [rolls (rolls input)
+        boards (boards input)]
+    [rolls boards]))
+
+(build-game demo-input)
